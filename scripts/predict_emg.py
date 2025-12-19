f349e5e7022587a3d2abc26925b34f777ca7dde4
masorol <miasorola.yoshida@gmail.com>
2025-12-19 16:06:39 -0600

    Add features and create predict_emg

--- scripts/predict_emg.py
+++ scripts/predict_emg.py
@@ -1,36 +1,45 @@
 import pandas as pd
 import numpy as np
+import joblib
 
-df = pd.read_csv("filtered_emg.csv")
-channel_cols = [c for c in df.columns if c.startswith("channel")]
+raw_df = pd.read_csv(".csv") # Read in raw data
 
 WINDOW_SIZE = 200
 STEP = 50
-features = []
+channel_prefix = "channel"
 
-for label, group in df.groupby('control_label'):
-    data = group[channel_cols].values
+def extract_features_from_df(df):
+    channel_cols = [c for c in df.columns if c.startswith(channel_prefix)]
+    data = df[channel_cols].values
     n = len(data)
+    features = []
+
     for start in range(0, n - WINDOW_SIZE + 1, STEP):
         window = data[start:start + WINDOW_SIZE, :]
 
         feat = {}
         for i, ch in enumerate(channel_cols):
-            x = window[:, i]
+            x  = window[:, i]
             dx = np.diff(x)
             feat[f'{ch}_RMS'] = np.sqrt(np.mean(x**2))
             feat[f'{ch}_MAV'] = np.mean(np.abs(x))
             feat[f'{ch}_WL']  = np.sum(np.abs(np.diff(x)))
-            
-            # new lines
             feat[f'{ch}_VAR'] = np.var(x)
             feat[f'{ch}_ZCR'] = np.sum((x[:-1] * x[1:]) < 0)
             feat[f'{ch}_SSC'] = np.sum((dx[:-1] * dx[1:]) < 0)
-            # here
 
-        feat['control_label'] = label
         features.append(feat)
 
-feat_df = pd.DataFrame(features)
-feat_df.to_csv("emg_car_windows_3class.csv", index=False)
-print(feat_df['control_label'].value_counts())
+    return pd.DataFrame(features)
+
+scaler = joblib.load("emg_3class_scaler.pkl")
+clf = joblib.load("emg_3class_logreg.pkl")
+
+X_new = extract_features_from_df(raw_df)
+X_new_s = scaler.transform(X_new)
+
+y_pred = clf.predict(X_new_s)
+y_pred_proba = clf.predict_proba(X_new_s)
+
+X_new["pred_label"] = y_pred
+X_new.to_csv("filtered_emg_pred_windows.csv", index=False)
